1cb7565871798a00ecbd3d205b2a60dda2a3d853
Lucas Jensen <lucas@lucasjensen.me>
2025-12-31 18:33:53 -0800

    Add new origin URL for CORS configuration

--- server/app/origins.py
+++ server/app/origins.py
@@ -2,6 +2,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173/",
     "https://mj.lucasjensen.me",
+    "https://meganjohns.com",
     "https://meganjohns.com/",
     "https://www.meganjohns.com",
     "https://www.meganjohns.com/",
