93a1951c3c0ff5c11177e46bd9b25952e33d3bef
Lucas Jensen <lucas@lucasjensen.me>
2026-09-17 19:02:18 -0700

    add reel, upgrade dependencies

---
+++ client/src/Reel/Reel.tsx
@@ -0,0 +1,14 @@
+export default function Reel() {
+  return (
+    <div className="youtube-player-container container">
+      <iframe
+        id="reel"
+        src="https://player.cloudinary.com/embed/?cloud_name=dreftv0ue&public_id=MeganJohns%2FUnicorns_Reel_9-15-2026_msxqmf"
+        width="640"
+        height="360"
+        allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
---
+++ server/README.md
@@ -0,0 +1,11 @@
+Manual virtual environment creation is not necessary, Poetry will handle that.
+
+Install dependencies
+```sh
+poetry install
+```
+
+See the database
+```sh
+poetry run seed
+```
